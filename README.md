331d4ced80dcd2cd7129d274b112d620310505aa
ruslanvaliullin <r.valiullin77@gmail.com>
2017-07-25 16:16:25 +0200

    Some little changes in DHT lib
    PUBLISHED_FROM=ef3a14fd5626f9b4027ae0623c978fd128ae831e

---
+++ README.md
@@ -0,0 +1,14 @@
+# Mongoose OS DHT sensor usage example (JavaScript)
+
+## Overview
+
+This example shows how to use DHT sensors on Mongoose OS in JavaScript.
+
+## How to install this app
+
+- Install and start [mos tool](https://mongoose-os.com/software.html)
+- Switch to the Project page, find and import this app, build and flash it:
+
+<p align="center">
+  <img src="https://mongoose-os.com/images/app1.gif" width="75%">
+</p>
---
+++ fs/init.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2014-2017 Cesanta Software Limited
+ * All rights reserved
+ *
+ * This example demonstrates how to use mJS DHT library API
+ * to get data from DHTxx temperature and humidity sensors.
+ */
+
+// Load Mongoose OS API
+load('api_timer.js');
+load('api_dht.js');
+
+// GPIO pin which has a DHT sensor data wire connected
+let pin = 16;
+
+// Initialize DHT library
+let dht = DHT.create(pin, DHT.DHT22);
+
+// This function reads data from the DHT sensor every 2 second
+Timer.set(2000 /* milliseconds */, true /* repeat */, function() {
+  let t = dht.getTemp();
+  let h = dht.getHumidity();
+
+  if (isNaN(h) || isNaN(t)) {
+    print('Failed to read data from sensor');
+    return;
+  }
+
+  print('Temperature:', t, '*C');
+  print('Humidity:', h, '%');
+}, null);
